8378111cad9c28777fa07abb2f29274bdca7b571
Augusto <augustoprados91@gmail.com>
2024-04-24 11:30:45 -0300

    fixes product dto

--- AP-Server/src/dto/product.dto.js
+++ AP-Server/src/dto/product.dto.js
@@ -5,10 +5,11 @@ class ProductDTO {
     constructor(data) {
         argsUtil.env !== "prod" && (this._id = crypto.randomBytes(12).toString("hex"));
         this.title = data.title;
-        this.photo = data.photo || "https://i.postimg.cc/HxdvTwqJ/events.jpg";
-        this.stock = data.stock;
-        this.price = data.price || 10;
         this.category = data.category;
+        this.photo = data.photo || "https://static.frieze.com/files/inline-images/new-site2.jpeg";
+        this.price = data.price;
+        this.stock = data.stock;
+        this.owner_id = data.owner_id;
         this.date = data.date || new Date();
         argsUtil.env !== "prod" && (this.updatedAt = new Date());
         argsUtil.env !== "prod" && (this.createdAt = new Date());
