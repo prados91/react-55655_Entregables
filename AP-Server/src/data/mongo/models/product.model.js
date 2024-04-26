414c7a5e280bfaec09664a1a606f15ada31c4ce9
Augusto <augustoprados91@gmail.com>
2024-04-26 14:48:50 -0300

    Fixes products default link

--- AP-Server/src/data/mongo/models/product.model.js
+++ AP-Server/src/data/mongo/models/product.model.js
@@ -6,7 +6,7 @@ const schema = new Schema(
     {
         title: { type: String, required: true },
         category: { type: String, required: true },
-        photo: { type: String, default: "https://i.postimg.cc/wTgNFWhR/profile.png" },
+        photo: { type: String, default: "https://i.postimg.cc/rmgbCmG5/default-Product-Image.jpg" },
         price: { type: Number, default: 1000 },
         stock: { type: Number, default: 50 },
         owner_id: { type: Types.ObjectId, ref: "users" },
